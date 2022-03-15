db45bba47f2d12efc83232330461ef3f79d4cfa6
aidyn <aidynsemey7sk@gmail.com>
2022-03-15 21:53:06 +0600

    permissions

--- backend/settings.py
+++ backend/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
